b5788a09206554a92b891c0780f2c4a99169698f
Dmitrii Kalinkin <kdvlad028@gmail.com>
2024-01-31 23:10:51 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$a_bool = true;   // логическое значение
+$a_str  = "foo";  // строка
+$a_str2 = 'foo';  // строка
+$an_int = 12;     // целое число
+
+echo get_debug_type($a_bool), "\n";
+echo get_debug_type($a_str), "\n";
+
+// Если это целое число, увеличить на четыре
+if (is_int($an_int)) {
+    $an_int += 4;
+}
+var_dump($an_int);
+
+// Если $a_bool - это строка, вывести её
+if (is_string($a_bool)) {
+    echo "Строка: $a_bool";
+}
+?>
